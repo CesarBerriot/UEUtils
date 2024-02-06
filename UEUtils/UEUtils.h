d30dd59ba1075cdbf2d391165fa081d8b64e1135
Nepsis <93186075+Nepsis7@users.noreply.github.com>
2024-02-06 11:49:01 +0100

    .

--- UEUtils/UEUtils.h
+++ UEUtils/UEUtils.h
@@ -22,10 +22,12 @@ namespace UEUtils
 	template <class T = UEUtilsDefaultGSType> FORCEINLINE T * GetGameState(ARG_WORLD);
 	template <class T = UEUtilsDefaultGameInstanceType> FORCEINLINE T * GetGameInstance(ARG_WORLD);
 	template <class T = UEUtilsDefaultHUDType> FORCEINLINE T * GetHUD(ARG_WORLD);
+	template <class T = APawn> FORCEINLINE T * GetPlayer(ARG_WORLD);
 	template <class T = UEUtilsDefaultGMType> DEFINE_INLINE_CHECKED_GETTER(T, GameMode, <T>)
 	template <class T = UEUtilsDefaultGSType> DEFINE_INLINE_CHECKED_GETTER(T, GameState, <T>)
 	template <class T = UEUtilsDefaultGameInstanceType> DEFINE_INLINE_CHECKED_GETTER(T, GameInstance, <T>)
 	template <class T = UEUtilsDefaultHUDType> DEFINE_INLINE_CHECKED_GETTER(T, HUD, <T>)
+	template <class T = APawn> DEFINE_INLINE_CHECKED_GETTER(T, Player, <T>)
 	DEFINE_INLINE_CHECKED_GETTER(UEnhancedInputLocalPlayerSubsystem, EnhancedInputLocalPlayerSubsystem)
 	void QuitGame(UWorld const * const & _world);
 };
@@ -53,7 +55,7 @@ DEFINE_SIMPLE_GETTER(GetGameMode, AGameModeBase, return TCAST(_world->GetAuthGam
 DEFINE_SIMPLE_GETTER(GetGameState, AGameStateBase, return TCAST(_world->GetGameState()))
 DEFINE_SIMPLE_GETTER(GetGameInstance, UGameInstance, return TCAST(_world->GetGameInstance()))
 DEFINE_SIMPLE_GETTER(GetHUD, AHUD, if(APlayerController const * const _pc = _world->GetFirstPlayerController()) return TCAST(_pc->GetHUD()))
-
+DEFINE_SIMPLE_GETTER(GetPlayer, APawn, if(APlayerController const * const _pc = _world->GetFirstPlayerController()) return TCAST(_pc->GetPawn()))
 
 UE_POP_MACRO("TEMPLATE_DEFAULT")
 UE_POP_MACRO("ARG_WORLD")
